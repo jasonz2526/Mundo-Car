c5fecd3f5efc57d7f2607c74b6ca77887834af7f
Jason Zhao <jaosnz2526@gmail.com>
2024-07-11 00:21:07 -0400

    updated background css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 import PlayerStats from './PlayerStats';
-import splashArts from './splashArts';
 import axios from 'axios';
+import splashArtsData from './splash_arts.json';
 
 /*function App() {
   return (
@@ -15,24 +15,7 @@ import axios from 'axios';
     </div>
   );
 }*/
-const championNames = [
-  'N/A', 'Aatrox', 'Ahri', 'Akali', 'Akshan', 'Alistar', 'Amumu', 'Anivia', 'Annie', 'Aphelios', 'Ashe', 'Aurelion Sol',
-  'Azir', 'Bard', 'Bel\'Veth', 'Blitzcrank', 'Brand', 'Braum', 'Briar', 'Caitlyn', 'Camille', 'Cassiopeia', 'Cho\'Gath',
-  'Corki', 'Darius', 'Diana', 'Dr. Mundo', 'Draven', 'Ekko', 'Elise', 'Evelynn', 'Ezreal', 'Fiddlesticks',
-  'Fiora', 'Fizz', 'Galio', 'Gangplank', 'Garen', 'Gnar', 'Gragas', 'Graves', 'Gwen', 'Hecarim', 'Heimerdinger', 'Hwei',
-  'Illaoi', 'Irelia', 'Ivern', 'Janna', 'Jarvan IV', 'Jax', 'Jayce', 'Jhin', 'Jinx', 'K\'Sante', 'Kai\'Sa',
-  'Kalista', 'Karma', 'Karthus', 'Kassadin', 'Katarina', 'Kayle', 'Kayn', 'Kennen', 'Kha\'Zix', 'Kindred',
-  'Kled', 'Kog\'Maw', 'LeBlanc', 'Lee Sin', 'Leona', 'Lillia', 'Lissandra', 'Lucian', 'Lulu', 'Lux',
-  'Malphite', 'Malzahar', 'Maokai', 'Master Yi', 'Milio', 'Miss Fortune', 'Mordekaiser', 'Morgana', 'Naafiri', 'Nami', 'Nasus', 'Nautilus',
-  'Neeko', 'Nidalee', 'Nilah', 'Nocturne', 'Nunu & Willump', 'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 'Pyke',
-  'Qiyana', 'Quinn', 'Rakan', 'Rammus', 'Rek\'Sai', 'Rell', 'Renata Glasc', 'Renekton', 'Rengar', 'Riven', 'Rumble',
-  'Ryze', 'Samira', 'Sejuani', 'Senna', 'Seraphine', 'Sett', 'Shaco', 'Shen', 'Shyvana', 'Singed',
-  'Sion', 'Sivir', 'Skarner', 'Smolder', 'Sona', 'Soraka', 'Swain', 'Sylas', 'Syndra', 'Tahm Kench', 'Taliyah',
-  'Talon', 'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'Twisted Fate', 'Twitch', 'Udyr',
-  'Urgot', 'Varus', 'Vayne', 'Veigar', 'Vel\'Koz', 'Vex', 'Vi', 'Viego', 'Viktor', 'Vladimir', 'Volibear',
-  'Warwick', 'Wukong', 'Xayah', 'Xerath', 'Xin Zhao', 'Yasuo', 'Yone', 'Yorick', 'Yuumi', 'Zac',
-  'Zed', 'Zeri', 'Ziggs', 'Zilean', 'Zoe', 'Zyra'
-];
+const championNames = ['N/A', ...Object.keys(splashArtsData)];
 
 const regionOptions = [
   { value: 'NA', label: 'North America' },
@@ -53,9 +36,20 @@ const roleOptions = [
 ]
 
 function App() {
-  const [backgroundImage, setBackgroundImage] = useState(splashArts[0]);
+  //const [backgroundImage, setBackgroundImage] = useState(splashArts[0]);
+  //const [backgroundImage, setBackgroundImage] = useState('');
+  //const [splashArts, setSplashArts] = useState([]);
+  const evelynnSplashArts = splashArtsData['Evelynn'];
+  const initialBackgroundImage = evelynnSplashArts[0];
+  const [backgroundImage, setBackgroundImage] = useState(initialBackgroundImage);
+  const [splashArts, setSplashArts] = useState(evelynnSplashArts);
+  const [summonerName, setSummonerName] = useState('');
+  const [tagline, setTagline] = useState('');
+  const [role, setRoles] = useState('');
+  const [region, setRegion] = useState('');
+  const [selectedChampion, setSelectedChampion] = useState({ label: 'Evelynn', value: 'Evelynn' });
 
-  useEffect(() => {
+  /*useEffect(() => {
     const interval = setInterval(() => {
       setBackgroundImage((prevImage) => {
         //const currentIndex = splashArts.indexOf(prevImage);
@@ -67,13 +61,24 @@ function App() {
     }, 10000); // Change the background every 10 seconds
 
     return () => clearInterval(interval);
-  }, []);
+  }, []);*/
 
-  const [summonerName, setSummonerName] = useState('');
-  const [tagline, setTagline] = useState('');
-  const [role, setRoles] = useState('');
-  const [region, setRegion] = useState('');
-  const [selectedChampion, setSelectedChampion] = useState('');
+  useEffect(() => {
+    if (selectedChampion.value && splashArtsData[selectedChampion.value]) {
+      setSplashArts(splashArtsData[selectedChampion.value]);
+    }
+  }, [selectedChampion]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (splashArts.length > 0) {
+        const randomIndex = Math.floor(Math.random() * splashArts.length);
+        setBackgroundImage(splashArts[randomIndex]);
+      }
+    }, 5000); // Change background every 10 seconds
+
+    return () => clearInterval(interval);
+  }, [splashArts]);
 
   const handleSummonerNameChange = (event) => {
     setSummonerName(event.target.value);
